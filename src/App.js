dc391e3d5b6a8c92ff69d8eee167d90429e0427d
Abhi <abhiramims333@gmail.com>
2024-05-13 21:33:07 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Todos from "./ToDos";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, content: "Call David" },
+    { id: 2, content: "Order supplies" }
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="Title">Todo's</h1>
+      <Todos todos={todos}/>
     </div>
   );
 }
---
+++ src/ToDos.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Todos = ({todos}) => {
+    const todoList = todos.length ? (
+        todos.map(todo => {
+            return(
+                <div key={todo.id}>
+                    <span>{todo.content}</span>
+                </div>
+            )
+        })
+    ) : (
+        
+        <p>You have no todos left</p>
+    ) 
+    return(
+        <div className="todos">
+        {todoList}
+        </div>
+    )
+}
+
+export default Todos;
